ee0d48067117a0dbdb541905efe0789322a33f0d
Galeteanu13 <nicolas.galeteanu@gmail.com>
2025-04-23 16:47:49 -0400

    Finally holly shit it works True initial commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleLinearCode/ExampleOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleLinearCode/ExampleOpMode_Linear.java
@@ -1,17 +1,22 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.ExampleLinearCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-@TeleOp(group = "Linear OpMode", name = "ExampleOpMode")
+@TeleOp(group = "R3P2 Examples", name = "ExampleOpMode")
 public class ExampleOpMode_Linear extends LinearOpMode {
     DcMotor shootMotor;
     @Override
     public void runOpMode() throws InterruptedException {
         shootMotor = hardwareMap.get(DcMotor.class, "ShootMotor");
         waitForStart();
+
         while(opModeIsActive()){
-            shootMotor.setPower(1);
+            if(gamepad1.a) {
+                shootMotor.setPower(1);
+            } else{
+                shootMotor.setPower(0);
+            }
         }
     }
 }
